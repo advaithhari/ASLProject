3131bc6185aa432b008cbf2a9442aed7f9ef4aa1
advaithhari <33227909+advaithhari@users.noreply.github.com>
2020-11-02 18:30:53 -0500

    bruh

---
+++ script.js
@@ -0,0 +1,41 @@
+let b = document.querySelector(".bruh");   
+  let butt = document.querySelector(".hehe")
+   
+
+   
+   
+   // Your web app's Firebase configuration
+   var firebaseConfig = {
+    apiKey: "AIzaSyDiEMKFF3n5hSDBb6xLsXoHclz_6bK_9wk",
+    authDomain: "aslproject-7fabb.firebaseapp.com",
+    databaseURL: "https://aslproject-7fabb.firebaseio.com",
+    projectId: "aslproject-7fabb",
+    storageBucket: "aslproject-7fabb.appspot.com",
+    messagingSenderId: "4437569972",
+    appId: "1:4437569972:web:70d9da46cbd6d2f97e42e1"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+
+  let database = firebase.database().ref();
+  console.log(database);
+
+  database.on('child_added', (data) => {
+let h = document.createElement("p");
+
+
+console.log(data.val().Character);
+ h.innerHTML  = data.val().Character;
+  document.body.appendChild(h);
+
+
+
+});
+
+butt.addEventListener("click",() => {
+  database.remove();
+ 
+    location.reload();
+
+});
+
