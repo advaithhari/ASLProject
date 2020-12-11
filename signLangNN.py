b2f9890c0519c87e6aabeb36c607f53867a84b5f
Vijay-2021 <vshah-21@peddie.org>
2020-12-11 11:43:20 -0500

    =add code for other dataset

--- signLangNN.py
+++ signLangNN.py
@@ -4,64 +4,70 @@ import tensorflow as tf
 import pathlib
 import matplotlib.pyplot as plt
 from tensorflow.keras import layers
+import pandas as pd
 
-batch_size = 10
+batch_size = 64
 img_height = 200
 img_width = 200
 
 
-data_dir = pathlib.Path("/home/jetsonnano/tensorTests/sign-language-alphabet-recognizer/dataset/")
-print(data_dir)
+#data_dir = pathlib.Path("/home/jetsonnano/tensorTests/sign-language-alphabet-recognizer/dataset/")
+#print(data_dir)
 
-image_count = len(list(data_dir.glob('*/*.jpg')))
-print(image_count)
+#image_count = len(list(data_dir.glob('*/*.jpg')))
+#print(image_count)
 
-data_augmentation = tf.keras.Sequential([
-  layers.experimental.preprocessing.RandomFlip("horizontal_and_vertical"),
-  layers.experimental.preprocessing.RandomRotation(0.2),
-])
+#data_augmentation = tf.keras.Sequential([
+ # layers.experimental.preprocessing.RandomFlip("horizontal_and_vertical"),
+#  layers.experimental.preprocessing.RandomRotation(0.2),
+#])
 
-train_ds = tf.keras.preprocessing.image_dataset_from_directory(
-  data_dir,
-  labels="inferred",
-  label_mode='int',
-  validation_split=0.2,
-  subset="training",
-  seed=123,
-  image_size=(img_height, img_width),
-  batch_size=batch_size)
+#train_ds = tf.keras.preprocessing.image_dataset_from_directory(
+ # data_dir,
+ # labels="inferred",
+ # label_mode='int',
+ # validation_split=0.2,
+ # subset="training",
+ # seed=123,
+ # image_size=(img_height, img_width),
+ # batch_size=batch_size)
 
-class_names = ['A','B','C']
+#class_names = ['A','B','C']
 
-val_ds = tf.keras.preprocessing.image_dataset_from_directory(
-  data_dir,
-  labels='inferred',
-  label_mode='int',
-  validation_split=0.2,
-  subset="validation",
-  seed=123,
-  image_size=(img_height, img_width),
-  batch_size=batch_size)
+#val_ds = tf.keras.preprocessing.image_dataset_from_directory(
+#  data_dir,
+#  labels='inferred',
+#  label_mode='int',
+#  validation_split=0.2,
+#  subset="validation",
+#  seed=123,
+ # image_size=(img_height, img_width),
+ # batch_size=batch_size)
 
 
-num_classes = 3
+num_classes = 25 # no J and Z bc requires motion 
+
+data = pd.read_csv('sign_mnist_train.csv')
+
+train=data.values[0:,1:] # all columns 0->n, all rows 1->k
+labels = data.values[0:,0] #all columns 0->n, 0th row
+
+train = train/255 #normalize 
+
+train = train.reshape(27455,28,28,1) # originally should be 27455, 784 for single pixel, we reshape into images
 
 model = tf.keras.Sequential([
-  layers.experimental.preprocessing.Rescaling(1./255),
-  layers.Conv2D(32, 5, padding='same',  activation='relu'),
+  layers.experimental.preprocessing.Resizing(28,28),
+  layers.experimental.preprocessing.RandomZoom(.3, .3),
+  layers.experimental.preprocessing.RandomRotation(.1),
+  layers.Conv2D(32, 3, padding='same',  activation='relu'),
   layers.MaxPooling2D(),
-  layers.Conv2D(64, 5,padding='same',  activation='relu'),
+  layers.Conv2D(64, 3,padding='same',  activation='relu'),
   layers.MaxPooling2D(),
-  layers.Conv2D(128, 5,padding='same',  activation='relu'),
-  layers.MaxPooling2D(),
-  layers.Conv2D(256, 5,padding='same',  activation='relu'),
-  layers.MaxPooling2D(),
-  layers.Conv2D(512, 5,padding='same',  activation='relu'),
-  layers.MaxPooling2D(),
-  layers.Conv2D(1024, 5, padding='same', activation='relu'),
+  layers.Conv2D(128, 3,padding='same',  activation='relu'),
   layers.MaxPooling2D(),
   layers.Flatten(),
-  layers.Dense(128, activation='relu'),
+  layers.Dense(512, activation='relu'),
   layers.Dense(num_classes,activation='softmax')
 ])
 
@@ -72,11 +78,12 @@ model.compile(
   metrics=['accuracy'])
 
 model.fit(
-  train_ds,
-  validation_data=val_ds,
-  epochs=1
+  train,
+  labels,
+  validation_split=0.25,
+  epochs=5
 )
-model.save("secondSignLangNNModel")
+model.save("thirdSignLangNNModel")
 
 acc = history.history['accuracy']
 val_acc = history.history['val_accuracy']
